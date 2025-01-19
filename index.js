d8251f07043d8a500c35ef897c2aacff00a93ad0
brandon <brandonawilliams1@gmail.com>
2025-01-19 16:36:14 -0600

    while loop added to run game while condition is true

--- index.js
+++ index.js
@@ -2,4 +2,15 @@ const minNum = 1000;
 const maxNum =10000;
 const answer = Math.floor(Math.random() * (maxNum-minNum + 1)) + minNum;
 
-console.log(answer);
+let attempts = 0;
+let guess;
+let running = true;
+
+while(running){
+
+        guess = window.prompt(`Pick 4 numbers for your shot at winning $10,000`);
+        guess = Number(guess);
+        running = false;
+    
+};
+

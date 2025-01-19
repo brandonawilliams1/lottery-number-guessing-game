895193a84501ad6c62b124e5a652c149cf02de7c
brandon <brandonawilliams1@gmail.com>
2025-01-19 17:11:58 -0600

    added if/else to check user input to generated number

--- index.js
+++ index.js
@@ -6,11 +6,33 @@ let attempts = 0;
 let guess;
 let running = true;
 
+//condition to run game
 while(running){
 
         guess = window.prompt(`Pick 4 numbers for your shot at winning $10,000`);
         guess = Number(guess);
-        running = false;
+        console.log(typeof(guess),guess);
+
+        //condition to check if input is a number and if input is a match
+        if(isNaN(guess)){
+           window.alert("please enter 4 valid numbers!");
+        } else if(guess < minNum || guess > maxNum){
+            window.alert("please enter 4 valid numbers!");
+        }
+        else{
+            attempts++;
+            if(guess < answer){
+                window.alert(`Your Numbers ${guess} do not match the draw ${answer} better luck next time`);
+            } else if( guess > answer){
+                window.alert(`Your Numbers ${guess} do not match the draw ${answer} better luck next time`);
+            } else{
+                window.alert(`Your Numbers ${guess} match the lucky numbers ${answer} you have won $10,000 `);
+                running = false;
+            }
+        }
+        
+        
+        
     
 };
 

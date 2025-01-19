d06adb6e4976ef4c6df58bb887f8d6ffa64a318a
brandon <brandonawilliams1@gmail.com>
2025-01-19 17:38:26 -0600

    set if/else block to continuously generate new lottery numbers after every attempt

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const minNum = 1000;
 const maxNum =10000;
-const answer = Math.floor(Math.random() * (maxNum-minNum + 1)) + minNum;
+let answer = Math.floor(Math.random() * (maxNum-minNum + 1)) + minNum;
 
 let attempts = 0;
 let guess;
@@ -16,23 +16,20 @@ while(running){
         //condition to check if input is a number and if input is a match
         if(isNaN(guess)){
            window.alert("please enter 4 valid numbers!");
-        } else if(guess < minNum || guess > maxNum){
+        } 
+        else if(guess < minNum || guess > maxNum){
             window.alert("please enter 4 valid numbers!");
         }
         else{
             attempts++;
-            if(guess < answer){
+            if(guess !== answer){
                 window.alert(`Your Numbers ${guess} do not match the draw ${answer} better luck next time`);
-            } else if( guess > answer){
-                window.alert(`Your Numbers ${guess} do not match the draw ${answer} better luck next time`);
-            } else{
-                window.alert(`Your Numbers ${guess} match the lucky numbers ${answer} you have won $10,000 `);
-                running = false;
+                answer = Math.floor(Math.random() * (maxNum-minNum + 1)) + minNum;
+            } else {
+                guess === answer
+                window.alert(`Your Numbers ${guess} match the draw ${answer} you're our lucky winner!!`);
             }
-        }
-        
-        
-        
+        };
+               
     
 };
-
